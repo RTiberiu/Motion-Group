43e5178600c1ac17444ac62da84ef1ce7526a3ae
Cassie Vicca <c.vicca@rgu.ac.uk>
2023-11-16 10:45:17 +0000

    basics for switch laid

---
+++ main.js
@@ -0,0 +1,93 @@
+function switchCase(called){
+switch(called){
+    case 1:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+    
+    case 2:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 3:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 4:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 5:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 6:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 7:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 8:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 9:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 10:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 11:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 12:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 13:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 14:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 15:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 16:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 17:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;
+
+    case 18:
+        element=document.querySelector(".selected");
+        element.classList.replace("oldClass","");
+        break;                                                                                      
+}
+}
