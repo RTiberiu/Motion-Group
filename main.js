0c2fa245d6086d2f560c0cb54212980913e7aaeb
littledoot <cassandravicca@gmail.com>
2023-11-16 23:18:53 +0000

    Button  switching done

--- main.js
+++ main.js
@@ -1,136 +1,88 @@
-function switchCase(called){
-switch(called){
-    case 1: /*Switch to layer2List1 */
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#1");
-        newSelect.classList.replace("","selected");
-        /*TODO: figure out how to add check again*/
+function switchCase(layer,cell){
+switch(layer){
+    case 1: /*Top layer buttons */
+        oldSelect=document.querySelector(".selectedTop");
+        oldSelect.classList.toggle("selectedTop");
+        newSelect=document.getElementById(cell);
+        newSelect.classList.toggle("selectedTop");
+        
+        /*showing and hiding layer 3, resets show sizes*/
+        if(cell =="configurator"){
+            showSmol=document.getElementById("layer2List2");
+            showSmol.classList.remove("smolhidden");
+            hideSmol=document.getElementById("layer2List1");
+            hideSmol.classList.add("smolhidden");
+
+            size=document.getElementById("show-size1");
+            size.classList.remove("selected");
+            size=document.getElementById("show-size2");
+            size.classList.remove("selected");
+
+            /*uncomment if you want selected to follow
+            oldSelect=document.querySelector(".selectedMid");
+            oldSelect.classList.toggle("selectedMid");
+            newSelect=document.getElementById("top-down");
+            newSelect.classList.toggle("selectedMid");
+            */
+        }
+        else{
+            showSmol=document.getElementById("layer2List2");
+            showSmol.classList.add("smolhidden");
+            hideSmol=document.getElementById("layer2List1");
+            hideSmol.classList.remove("smolhidden");
+
+            size=document.getElementById("show-size1");
+            size.classList.remove("selected");
+            size=document.getElementById("show-size2");
+            size.classList.remove("selected");
+
+            /*uncomment if you want selected to follow
+            oldSelect=document.querySelector(".selectedMid");
+            oldSelect.classList.toggle("selectedMid");
+            newSelect=document.getElementById("side1");
+            newSelect.classList.toggle("selectedMid");
+            */
+        }
+        
         break;
     
     case 2: /*Switch to layer2List2 */
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#2");
-        newSelect.classList.replace("","selected");
-        newShowSmol=document.querySelector("#layer2List2");
-        newShowSmol.classList.replace("smolhidden","");
+        oldSelect=document.querySelector(".selectedMid");
+        oldSelect.classList.toggle("selectedMid");
+        newSelect=document.getElementById(cell);
+        newSelect.classList.toggle("selectedMid");
+
+        if(cell =="inside"){
+            console.log("in if");
+            showBig=document.getElementById("layer3");
+            showBig.classList.remove("bighidden");
+            showSmol=document.getElementById("layer3List");
+            showSmol.classList.remove("smolhidden");
+        }
+        else{
+            console.log("in else");
+            showBig=document.getElementById("layer3");
+            showBig.classList.add("bighidden");
+            showSmol=document.getElementById("layer3List");
+            showSmol.classList.add("smolhidden");
+        }
         break;
 
     case 3:/*buttons 3-8 are different OUTSIDE views */
         oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#3");
-        newSelect.classList.replace("","selected");
+        oldSelect.classList.toggle("selected");
+        newSelect=document.getElementById(cell);
+        newSelect.classList.toggle("selected");
         break;
-
+        
     case 4:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#4");
-        newSelect.classList.replace("","selected");
+        size=document.getElementById(cell);
+        if(size.classList.contains("selected")){
+            size.classList.remove("selected");
+        }
+        else{
+            size.classList.add("selected");
+        }
         break;
-
-    case 5:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#5");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 6:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#6");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 7:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#7");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 8:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#8");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 9: /*Switch to inside view and reveal layer2List*/
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#9");
-        newSelect.classList.replace("","selected");
-        newShowBig=document.querySelector("#layer3");
-        newShowBig.classList.replace("bighidden","")
-        newShowSmol=document.querySelector("#layer3List");
-        newShowSmol.classList.replace("smolhidden","")
-        break;
-
-    case 10:/*Show sizes (toggle?)*/
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#10");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 11: /*11-14 layer2List2*/
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#11");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 12:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#12");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 13:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#13");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 14:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#14");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 15:/*layer3List*/
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#15");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 16:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#16");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 17:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#17");
-        newSelect.classList.replace("","selected");
-        break;
-
-    case 18:
-        oldSelect=document.querySelector(".selected");
-        oldSelect.classList.replace("selected","");
-        newSelect=document.querySelector("#18");
-        newSelect.classList.replace("","selected");
-        break;                                                                                      
-}
+    }
 }
