0c7e86859a5f1b039b1de824dd20013c850e9cbb
Cassie Vicca <c.vicca@rgu.ac.uk>
2023-11-16 13:18:30 +0000

    added id's for refs

--- main.js
+++ main.js
@@ -2,92 +2,92 @@ function switchCase(called){
 switch(called){
     case 1:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
     
     case 2:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 3:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 4:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 5:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 6:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 7:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 8:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 9:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 10:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 11:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 12:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 13:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 14:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 15:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 16:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 17:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;
 
     case 18:
         element=document.querySelector(".selected");
-        element.classList.replace("oldClass","");
+        element.classList.replace("selected","");
         break;                                                                                      
 }
 }
